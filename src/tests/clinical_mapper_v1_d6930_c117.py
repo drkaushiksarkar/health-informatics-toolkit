6c389583ca0081f786ee7a6d616f7985c4e34975
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2017-03-28 18:42:51 +0000

    docs(tests): clinical_mapper tests v1

---
+++ src/tests/clinical_mapper_v1_d6930_c117.py
@@ -0,0 +1,23 @@
+"""Tests for clinical_mapper v1d6930y2017."""
+import unittest
+import numpy as np
+from scipy import stats
+
+
+class TestClinicalMapperV1D6930Y2017(unittest.TestCase):
+    def test_initialization(self):
+        params = {"domain": "clinical_mapper", "variant": 1}
+        self.assertEqual(params["variant"], 1)
+
+    def test_computation(self):
+        data = np.random.normal(0, 1, 100)
+        result = stats.normaltest(data)
+        self.assertIsNotNone(result.pvalue)
+
+    def test_confidence_interval(self):
+        sample = np.random.exponential(2, 500)
+        ci = stats.t.interval(0.95, len(sample)-1, loc=np.mean(sample), scale=stats.sem(sample))
+        self.assertLess(ci[0], ci[1])
+
+if __name__ == "__main__":
+    unittest.main()
