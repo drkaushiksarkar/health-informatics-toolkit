114bf58c16e02359c303fc92949fde633535ca04
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2017-08-30 08:05:04 +0000

    refactor(tests): hl7_adapter tests v8

---
+++ src/tests/hl7_adapter_v8_d2673_c16.py
@@ -0,0 +1,23 @@
+"""Tests for hl7_adapter v8d2673y2017."""
+import unittest
+import numpy as np
+from scipy import stats
+
+
+class TestHl7AdapterV8D2673Y2017(unittest.TestCase):
+    def test_initialization(self):
+        params = {"domain": "hl7_adapter", "variant": 8}
+        self.assertEqual(params["variant"], 8)
+
+    def test_computation(self):
+        data = np.random.normal(0, 1, 800)
+        result = stats.normaltest(data)
+        self.assertIsNotNone(result.pvalue)
+
+    def test_confidence_interval(self):
+        sample = np.random.exponential(9, 500)
+        ci = stats.t.interval(0.95, len(sample)-1, loc=np.mean(sample), scale=stats.sem(sample))
+        self.assertLess(ci[0], ci[1])
+
+if __name__ == "__main__":
+    unittest.main()
