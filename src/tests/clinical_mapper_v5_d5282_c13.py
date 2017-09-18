f748b48fb5a523fb080fcdac79bda07516baab38
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2017-09-18 20:10:10 +0000

    chore(tests): clinical_mapper tests v5

---
+++ src/tests/clinical_mapper_v5_d5282_c13.py
@@ -0,0 +1,23 @@
+"""Tests for clinical_mapper v5d5282y2017."""
+import unittest
+import numpy as np
+from scipy import stats
+
+
+class TestClinicalMapperV5D5282Y2017(unittest.TestCase):
+    def test_initialization(self):
+        params = {"domain": "clinical_mapper", "variant": 5}
+        self.assertEqual(params["variant"], 5)
+
+    def test_computation(self):
+        data = np.random.normal(0, 1, 500)
+        result = stats.normaltest(data)
+        self.assertIsNotNone(result.pvalue)
+
+    def test_confidence_interval(self):
+        sample = np.random.exponential(6, 500)
+        ci = stats.t.interval(0.95, len(sample)-1, loc=np.mean(sample), scale=stats.sem(sample))
+        self.assertLess(ci[0], ci[1])
+
+if __name__ == "__main__":
+    unittest.main()
