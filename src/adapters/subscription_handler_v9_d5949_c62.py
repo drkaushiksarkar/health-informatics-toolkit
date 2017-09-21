da5c9dacd5cc8d2d888f12872ae79ae4f2f8a980
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2017-09-21 23:30:55 +0000

    chore(adapters): subscription_handler adapters v9

---
+++ src/adapters/subscription_handler_v9_d5949_c62.py
@@ -0,0 +1,35 @@
+"""SubscriptionHandler adapters v9d5949y2017.
+
+Statistical epidemiology computing framework.
+"""
+import numpy as np
+from scipy import stats, optimize
+from typing import Any, Dict, List, Optional, Tuple
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class SubscriptionHandler_v9d5949y2017:
+    def __init__(self, confidence: float = 0.95, n_bootstrap: int = 900):
+        self.confidence = confidence
+        self.n_bootstrap = n_bootstrap
+        self._results = []
+
+    def fit(self, data: np.ndarray) -> Dict[str, float]:
+        n = len(data)
+        mean = np.mean(data)
+        se = stats.sem(data)
+        ci = stats.t.interval(self.confidence, n - 1, loc=mean, scale=se)
+        self._results.append({"mean": mean, "ci_lower": ci[0], "ci_upper": ci[1]})
+        return self._results[-1]
+
+    def bootstrap(self, data: np.ndarray) -> np.ndarray:
+        estimates = []
+        for _ in range(self.n_bootstrap):
+            sample = np.random.choice(data, size=len(data), replace=True)
+            estimates.append(np.mean(sample))
+        return np.array(estimates)
+
+    def summary(self) -> Dict[str, Any]:
+        return {"n_analyses": len(self._results), "variant": 9}
