07d10bedc42595be659bf4478da648143f1b4c37
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2017-09-21 17:07:54 +0000

    docs(schemas): subscription_handler schemas v3

---
+++ src/schemas/subscription_handler_v3_d5949_c110.py
@@ -0,0 +1,21 @@
+"""SubscriptionHandler schemas v3d5949y2017."""
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+
+@dataclass
+class SubscriptionHandlerConfig_v3d5949y2017:
+    enabled: bool = True
+    batch_size: int = 96
+    hidden_dim: int = 192
+    num_layers: int = 5
+    dropout: float = 0.3
+    learning_rate: float = 3.0e-04
+    max_epochs: int = 30
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, Any]) -> "SubscriptionHandlerConfig_v3d5949y2017":
+        return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
+
+    def validate(self) -> bool:
+        return self.batch_size > 0 and self.hidden_dim > 0
