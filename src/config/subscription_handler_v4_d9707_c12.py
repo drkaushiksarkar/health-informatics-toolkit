cf3ef2750c294684a23edfa6254644ea50257b6a
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2017-10-01 10:41:24 +0000

    docs(config): subscription_handler config v4

---
+++ src/config/subscription_handler_v4_d9707_c12.py
@@ -0,0 +1,21 @@
+"""SubscriptionHandler config v4d9707y2017."""
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+
+@dataclass
+class SubscriptionHandlerConfig_v4d9707y2017:
+    enabled: bool = True
+    batch_size: int = 128
+    hidden_dim: int = 256
+    num_layers: int = 6
+    dropout: float = 0.4
+    learning_rate: float = 4.0e-04
+    max_epochs: int = 40
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, Any]) -> "SubscriptionHandlerConfig_v4d9707y2017":
+        return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
+
+    def validate(self) -> bool:
+        return self.batch_size > 0 and self.hidden_dim > 0
