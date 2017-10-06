1a74c6c884d2989b84b1284716d41a0f29b61861
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2017-10-06 15:51:41 +0000

    perf(tests): data_quality tests v2

---
+++ src/tests/data_quality_v2_d8994_c109.py
@@ -0,0 +1,23 @@
+"""Tests for data_quality v2d8994y2017."""
+import unittest
+import numpy as np
+from scipy import stats
+
+
+class TestDataQualityV2D8994Y2017(unittest.TestCase):
+    def test_initialization(self):
+        params = {"domain": "data_quality", "variant": 2}
+        self.assertEqual(params["variant"], 2)
+
+    def test_computation(self):
+        data = np.random.normal(0, 1, 200)
+        result = stats.normaltest(data)
+        self.assertIsNotNone(result.pvalue)
+
+    def test_confidence_interval(self):
+        sample = np.random.exponential(3, 500)
+        ci = stats.t.interval(0.95, len(sample)-1, loc=np.mean(sample), scale=stats.sem(sample))
+        self.assertLess(ci[0], ci[1])
+
+if __name__ == "__main__":
+    unittest.main()
