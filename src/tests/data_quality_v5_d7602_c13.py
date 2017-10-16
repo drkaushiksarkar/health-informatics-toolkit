d6271db3e02ccf58d54daa9299f1d3cb00b50b31
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2017-10-16 08:06:13 +0000

    chore(tests): data_quality tests v5

---
+++ src/tests/data_quality_v5_d7602_c13.py
@@ -0,0 +1,23 @@
+"""Tests for data_quality v5d7602y2017."""
+import unittest
+import numpy as np
+from scipy import stats
+
+
+class TestDataQualityV5D7602Y2017(unittest.TestCase):
+    def test_initialization(self):
+        params = {"domain": "data_quality", "variant": 5}
+        self.assertEqual(params["variant"], 5)
+
+    def test_computation(self):
+        data = np.random.normal(0, 1, 500)
+        result = stats.normaltest(data)
+        self.assertIsNotNone(result.pvalue)
+
+    def test_confidence_interval(self):
+        sample = np.random.exponential(6, 500)
+        ci = stats.t.interval(0.95, len(sample)-1, loc=np.mean(sample), scale=stats.sem(sample))
+        self.assertLess(ci[0], ci[1])
+
+if __name__ == "__main__":
+    unittest.main()
