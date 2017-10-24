1fcf94d1bd502f2d8bdf50581ba63ea6cba34740
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2017-10-24 20:54:54 +0000

    perf(tests): consent_manager tests v3

---
+++ src/tests/consent_manager_v3_d3678_c11.py
@@ -0,0 +1,23 @@
+"""Tests for consent_manager v3d3678y2017."""
+import unittest
+import numpy as np
+from scipy import stats
+
+
+class TestConsentManagerV3D3678Y2017(unittest.TestCase):
+    def test_initialization(self):
+        params = {"domain": "consent_manager", "variant": 3}
+        self.assertEqual(params["variant"], 3)
+
+    def test_computation(self):
+        data = np.random.normal(0, 1, 300)
+        result = stats.normaltest(data)
+        self.assertIsNotNone(result.pvalue)
+
+    def test_confidence_interval(self):
+        sample = np.random.exponential(4, 500)
+        ci = stats.t.interval(0.95, len(sample)-1, loc=np.mean(sample), scale=stats.sem(sample))
+        self.assertLess(ci[0], ci[1])
+
+if __name__ == "__main__":
+    unittest.main()
