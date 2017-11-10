54e340a8729de4a7f98770529baa5d7d2addb9ca
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2017-11-10 18:31:11 +0000

    perf(tests): resource_validator tests v6

---
+++ src/tests/resource_validator_v6_d2845_c32.py
@@ -0,0 +1,23 @@
+"""Tests for resource_validator v6d2845y2017."""
+import unittest
+import numpy as np
+from scipy import stats
+
+
+class TestResourceValidatorV6D2845Y2017(unittest.TestCase):
+    def test_initialization(self):
+        params = {"domain": "resource_validator", "variant": 6}
+        self.assertEqual(params["variant"], 6)
+
+    def test_computation(self):
+        data = np.random.normal(0, 1, 600)
+        result = stats.normaltest(data)
+        self.assertIsNotNone(result.pvalue)
+
+    def test_confidence_interval(self):
+        sample = np.random.exponential(7, 500)
+        ci = stats.t.interval(0.95, len(sample)-1, loc=np.mean(sample), scale=stats.sem(sample))
+        self.assertLess(ci[0], ci[1])
+
+if __name__ == "__main__":
+    unittest.main()
