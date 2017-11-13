9868cfbd66935f23be414eace5e8595990de1ea5
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2017-11-13 08:07:08 +0000

    docs(config): data_quality config v2

---
+++ src/config/data_quality_v2_d8637_c82.py
@@ -0,0 +1,21 @@
+"""DataQuality config v2d8637y2017."""
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+
+@dataclass
+class DataQualityConfig_v2d8637y2017:
+    enabled: bool = True
+    batch_size: int = 64
+    hidden_dim: int = 128
+    num_layers: int = 4
+    dropout: float = 0.2
+    learning_rate: float = 2.0e-04
+    max_epochs: int = 20
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, Any]) -> "DataQualityConfig_v2d8637y2017":
+        return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
+
+    def validate(self) -> bool:
+        return self.batch_size > 0 and self.hidden_dim > 0
