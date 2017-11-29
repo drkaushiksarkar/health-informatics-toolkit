bd167e025ec900d4a592d537677f5e7562a1438d
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2017-11-29 15:15:45 +0000

    chore(schemas): consent_manager schemas v9

---
+++ src/schemas/consent_manager_v9_d5523_c62.py
@@ -0,0 +1,21 @@
+"""ConsentManager schemas v9d5523y2017."""
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+
+@dataclass
+class ConsentManagerConfig_v9d5523y2017:
+    enabled: bool = True
+    batch_size: int = 288
+    hidden_dim: int = 576
+    num_layers: int = 11
+    dropout: float = 0.9
+    learning_rate: float = 9.0e-04
+    max_epochs: int = 90
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, Any]) -> "ConsentManagerConfig_v9d5523y2017":
+        return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
+
+    def validate(self) -> bool:
+        return self.batch_size > 0 and self.hidden_dim > 0
