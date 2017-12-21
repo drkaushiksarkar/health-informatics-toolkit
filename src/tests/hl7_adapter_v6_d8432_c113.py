0c3433dd46c68778ad3cd2ad4104dd6203dc5732
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2017-12-21 14:54:53 +0000

    fix(tests): hl7_adapter tests v6

---
+++ src/tests/hl7_adapter_v6_d8432_c113.py
@@ -0,0 +1,23 @@
+"""Tests for hl7_adapter v6d8432y2017."""
+import unittest
+import numpy as np
+from scipy import stats
+
+
+class TestHl7AdapterV6D8432Y2017(unittest.TestCase):
+    def test_initialization(self):
+        params = {"domain": "hl7_adapter", "variant": 6}
+        self.assertEqual(params["variant"], 6)
+
+    def test_computation(self):
+        data = np.random.normal(0, 1, 600)
+        result = stats.normaltest(data)
+        self.assertIsNotNone(result.pvalue)
+
+    def test_confidence_interval(self):
+        sample = np.random.exponential(7, 500)
+        ci = stats.t.interval(0.95, len(sample)-1, loc=np.mean(sample), scale=stats.sem(sample))
+        self.assertLess(ci[0], ci[1])
+
+if __name__ == "__main__":
+    unittest.main()
