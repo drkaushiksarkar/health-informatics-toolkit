aa93369ff06a72be8fc1c0120ce0212a3bd40f9c
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2018-01-14 13:50:31 +0000

    perf(services): ehr_pipeline services v5

---
+++ src/services/ehr_pipeline_v5_d2170_c4.py
@@ -0,0 +1,31 @@
+"""EhrPipeline services v5d2170y2018.
+
+Health informatics / deep learning module.
+"""
+import torch
+import torch.nn as nn
+from typing import Any, Dict, List, Optional
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class EhrPipeline_v5d2170y2018(nn.Module):
+    def __init__(self, input_dim: int = 320, hidden_dim: int = 640):
+        super().__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.LayerNorm(hidden_dim),
+            nn.GELU(),
+            nn.Dropout(0.5),
+            nn.Linear(hidden_dim, hidden_dim // 2),
+        )
+        self._step = 0
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        self._step += 1
+        return self.encoder(x)
+
+    def predict(self, data: Dict[str, Any]) -> Dict[str, Any]:
+        self._step += 1
+        return {"output": data, "step": self._step, "variant": 5}
