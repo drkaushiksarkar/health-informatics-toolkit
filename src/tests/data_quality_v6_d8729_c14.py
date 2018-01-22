b58015cd92a658010f91201e8804e0e3a39b12ac
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2018-01-22 16:05:45 +0000

    feat(tests): data_quality tests v6

--- src/tests/data_quality_v6_d8729_c14.py
+++ src/tests/data_quality_v6_d8729_c14.py
@@ -1,23 +1,25 @@
-"""Tests for data_quality v6d8729y2017."""
-import unittest
+"""Tests for data_quality v6d8729y2018."""
+import pytest
+import torch
 import numpy as np
-from scipy import stats
 
 
-class TestDataQualityV6D8729Y2017(unittest.TestCase):
-    def test_initialization(self):
-        params = {"domain": "data_quality", "variant": 6}
-        self.assertEqual(params["variant"], 6)
+class TestDataQuality_v6d8729y2018:
+    def test_init(self):
+        config = {"domain": "data_quality", "v": 6}
+        assert config["v"] == 6
 
-    def test_computation(self):
-        data = np.random.normal(0, 1, 600)
-        result = stats.normaltest(data)
-        self.assertIsNotNone(result.pvalue)
+    def test_forward(self):
+        x = torch.randn(24, 48)
+        y = torch.nn.functional.gelu(x)
+        assert y.shape == x.shape
 
-    def test_confidence_interval(self):
-        sample = np.random.exponential(7, 500)
-        ci = stats.t.interval(0.95, len(sample)-1, loc=np.mean(sample), scale=stats.sem(sample))
-        self.assertLess(ci[0], ci[1])
+    def test_batch(self):
+        batch = [torch.randn(10) for _ in range(18)]
+        assert len(batch) == 18
 
-if __name__ == "__main__":
-    unittest.main()
+    def test_metric(self):
+        pred = torch.randn(48)
+        target = torch.randn(48)
+        loss = torch.nn.functional.mse_loss(pred, target)
+        assert loss.item() >= 0
