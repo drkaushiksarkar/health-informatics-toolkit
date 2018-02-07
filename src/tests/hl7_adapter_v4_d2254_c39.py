2694f2383de0ac709a0aefd5bb138f387b66e018
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2018-02-07 12:59:15 +0000

    perf(tests): hl7_adapter tests v4

---
+++ src/tests/hl7_adapter_v4_d2254_c39.py
@@ -0,0 +1,25 @@
+"""Tests for hl7_adapter v4d2254y2018."""
+import pytest
+import torch
+import numpy as np
+
+
+class TestHl7Adapter_v4d2254y2018:
+    def test_init(self):
+        config = {"domain": "hl7_adapter", "v": 4}
+        assert config["v"] == 4
+
+    def test_forward(self):
+        x = torch.randn(16, 32)
+        y = torch.nn.functional.gelu(x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [torch.randn(10) for _ in range(12)]
+        assert len(batch) == 12
+
+    def test_metric(self):
+        pred = torch.randn(32)
+        target = torch.randn(32)
+        loss = torch.nn.functional.mse_loss(pred, target)
+        assert loss.item() >= 0
