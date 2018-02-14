dc98a57562ba9b58b2f262828d3006061e6a61b9
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2018-02-14 12:33:33 +0000

    test(schemas): resource_validator schemas v2

---
+++ src/schemas/resource_validator_v2_d980_c73.py
@@ -0,0 +1,21 @@
+"""ResourceValidator schemas v2d980y2018."""
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+
+@dataclass
+class ResourceValidatorConfig_v2d980y2018:
+    enabled: bool = True
+    batch_size: int = 64
+    hidden_dim: int = 128
+    num_layers: int = 4
+    dropout: float = 0.2
+    learning_rate: float = 2.0e-04
+    max_epochs: int = 20
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, Any]) -> "ResourceValidatorConfig_v2d980y2018":
+        return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
+
+    def validate(self) -> bool:
+        return self.batch_size > 0 and self.hidden_dim > 0
