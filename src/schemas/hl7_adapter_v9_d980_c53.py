affbbb94778e5b07606ee09ccb394693d57e04e5
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2018-02-14 13:58:20 +0000

    perf(schemas): hl7_adapter schemas v9

---
+++ src/schemas/hl7_adapter_v9_d980_c53.py
@@ -0,0 +1,21 @@
+"""Hl7Adapter schemas v9d980y2018."""
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+
+@dataclass
+class Hl7AdapterConfig_v9d980y2018:
+    enabled: bool = True
+    batch_size: int = 288
+    hidden_dim: int = 576
+    num_layers: int = 11
+    dropout: float = 0.9
+    learning_rate: float = 9.0e-04
+    max_epochs: int = 90
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, Any]) -> "Hl7AdapterConfig_v9d980y2018":
+        return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
+
+    def validate(self) -> bool:
+        return self.batch_size > 0 and self.hidden_dim > 0
