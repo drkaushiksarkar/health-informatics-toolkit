a58950bc76898065f6024989fc457057d59069d1
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2018-02-24 22:10:14 +0000

    feat(tests): data_quality tests v6

---
+++ src/tests/data_quality_v6_d8330_c77.py
@@ -0,0 +1,25 @@
+"""Tests for data_quality v6d8330y2018."""
+import pytest
+import torch
+import numpy as np
+
+
+class TestDataQuality_v6d8330y2018:
+    def test_init(self):
+        config = {"domain": "data_quality", "v": 6}
+        assert config["v"] == 6
+
+    def test_forward(self):
+        x = torch.randn(24, 48)
+        y = torch.nn.functional.gelu(x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [torch.randn(10) for _ in range(18)]
+        assert len(batch) == 18
+
+    def test_metric(self):
+        pred = torch.randn(48)
+        target = torch.randn(48)
+        loss = torch.nn.functional.mse_loss(pred, target)
+        assert loss.item() >= 0
