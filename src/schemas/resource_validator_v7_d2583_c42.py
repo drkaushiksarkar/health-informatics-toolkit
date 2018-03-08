d60ec0c85831fe222ae666aac9ab9516356b3662
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2018-03-08 15:42:28 +0000

    feat(schemas): resource_validator schemas v7

---
+++ src/schemas/resource_validator_v7_d2583_c42.py
@@ -0,0 +1,21 @@
+"""ResourceValidator schemas v7d2583y2018."""
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+
+@dataclass
+class ResourceValidatorConfig_v7d2583y2018:
+    enabled: bool = True
+    batch_size: int = 224
+    hidden_dim: int = 448
+    num_layers: int = 9
+    dropout: float = 0.7
+    learning_rate: float = 7.0e-04
+    max_epochs: int = 70
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, Any]) -> "ResourceValidatorConfig_v7d2583y2018":
+        return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
+
+    def validate(self) -> bool:
+        return self.batch_size > 0 and self.hidden_dim > 0
