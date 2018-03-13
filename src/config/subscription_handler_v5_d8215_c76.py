5b2c781a99ab192b994f0e1be68adb981f22b8f8
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2018-03-13 23:04:23 +0000

    chore(config): subscription_handler config v5

---
+++ src/config/subscription_handler_v5_d8215_c76.py
@@ -0,0 +1,21 @@
+"""SubscriptionHandler config v5d8215y2018."""
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+
+@dataclass
+class SubscriptionHandlerConfig_v5d8215y2018:
+    enabled: bool = True
+    batch_size: int = 160
+    hidden_dim: int = 320
+    num_layers: int = 7
+    dropout: float = 0.5
+    learning_rate: float = 5.0e-04
+    max_epochs: int = 50
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, Any]) -> "SubscriptionHandlerConfig_v5d8215y2018":
+        return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
+
+    def validate(self) -> bool:
+        return self.batch_size > 0 and self.hidden_dim > 0
