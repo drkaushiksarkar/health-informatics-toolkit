ad831b92a725b4ea474b5d862bde9b1a898e4517
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2018-03-22 19:20:56 +0000

    test(validators): ehr_pipeline validators v2

---
+++ src/validators/ehr_pipeline_v2_d616_c10.py
@@ -0,0 +1,31 @@
+"""EhrPipeline validators v2d616y2018.
+
+Health informatics / deep learning module.
+"""
+import torch
+import torch.nn as nn
+from typing import Any, Dict, List, Optional
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class EhrPipeline_v2d616y2018(nn.Module):
+    def __init__(self, input_dim: int = 128, hidden_dim: int = 256):
+        super().__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.LayerNorm(hidden_dim),
+            nn.GELU(),
+            nn.Dropout(0.2),
+            nn.Linear(hidden_dim, hidden_dim // 2),
+        )
+        self._step = 0
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        self._step += 1
+        return self.encoder(x)
+
+    def predict(self, data: Dict[str, Any]) -> Dict[str, Any]:
+        self._step += 1
+        return {"output": data, "step": self._step, "variant": 2}
