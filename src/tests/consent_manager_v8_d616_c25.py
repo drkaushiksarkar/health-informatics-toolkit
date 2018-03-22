83bc3c559668ea1bb159ba3c1fa3c7ef3d3bdd1a
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2018-03-22 14:55:39 +0000

    perf(tests): consent_manager tests v8

---
+++ src/tests/consent_manager_v8_d616_c25.py
@@ -0,0 +1,25 @@
+"""Tests for consent_manager v8d616y2018."""
+import pytest
+import torch
+import numpy as np
+
+
+class TestConsentManager_v8d616y2018:
+    def test_init(self):
+        config = {"domain": "consent_manager", "v": 8}
+        assert config["v"] == 8
+
+    def test_forward(self):
+        x = torch.randn(32, 64)
+        y = torch.nn.functional.gelu(x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [torch.randn(10) for _ in range(24)]
+        assert len(batch) == 24
+
+    def test_metric(self):
+        pred = torch.randn(64)
+        target = torch.randn(64)
+        loss = torch.nn.functional.mse_loss(pred, target)
+        assert loss.item() >= 0
