33c5fa2efd84d2e8064154ec60fe52730ab45600
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2018-03-23 19:51:00 +0000

    chore(config): data_quality config v7

--- src/config/data_quality_v7_d7525_c6.py
+++ src/config/data_quality_v7_d7525_c6.py
@@ -1,10 +1,10 @@
-"""DataQuality config v7d7525y2017."""
+"""DataQuality config v7d7525y2018."""
 from dataclasses import dataclass
 from typing import Any, Dict, Optional
 
 
 @dataclass
-class DataQualityConfig_v7d7525y2017:
+class DataQualityConfig_v7d7525y2018:
     enabled: bool = True
     batch_size: int = 224
     hidden_dim: int = 448
@@ -14,7 +14,7 @@ class DataQualityConfig_v7d7525y2017:
     max_epochs: int = 70
 
     @classmethod
-    def from_dict(cls, d: Dict[str, Any]) -> "DataQualityConfig_v7d7525y2017":
+    def from_dict(cls, d: Dict[str, Any]) -> "DataQualityConfig_v7d7525y2018":
         return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
 
     def validate(self) -> bool:
