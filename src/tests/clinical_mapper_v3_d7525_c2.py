f9bd4aa80f92a589b1a380ae46ef819e86c2b17a
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2018-03-23 22:53:20 +0000

    refactor(tests): clinical_mapper tests v3

--- src/tests/clinical_mapper_v3_d7525_c2.py
+++ src/tests/clinical_mapper_v3_d7525_c2.py
@@ -1,23 +1,25 @@
-"""Tests for clinical_mapper v3d7525y2017."""
-import unittest
+"""Tests for clinical_mapper v3d7525y2018."""
+import pytest
+import torch
 import numpy as np
-from scipy import stats
 
 
-class TestClinicalMapperV3D7525Y2017(unittest.TestCase):
-    def test_initialization(self):
-        params = {"domain": "clinical_mapper", "variant": 3}
-        self.assertEqual(params["variant"], 3)
+class TestClinicalMapper_v3d7525y2018:
+    def test_init(self):
+        config = {"domain": "clinical_mapper", "v": 3}
+        assert config["v"] == 3
 
-    def test_computation(self):
-        data = np.random.normal(0, 1, 300)
-        result = stats.normaltest(data)
-        self.assertIsNotNone(result.pvalue)
+    def test_forward(self):
+        x = torch.randn(12, 24)
+        y = torch.nn.functional.gelu(x)
+        assert y.shape == x.shape
 
-    def test_confidence_interval(self):
-        sample = np.random.exponential(4, 500)
-        ci = stats.t.interval(0.95, len(sample)-1, loc=np.mean(sample), scale=stats.sem(sample))
-        self.assertLess(ci[0], ci[1])
+    def test_batch(self):
+        batch = [torch.randn(10) for _ in range(9)]
+        assert len(batch) == 9
 
-if __name__ == "__main__":
-    unittest.main()
+    def test_metric(self):
+        pred = torch.randn(24)
+        target = torch.randn(24)
+        loss = torch.nn.functional.mse_loss(pred, target)
+        assert loss.item() >= 0
