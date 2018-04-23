dcb09aaa3ca931b5e824916c19ef1d8e360c59a2
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2018-04-23 20:40:15 +0000

    test(schemas): subscription_handler schemas v2

--- src/schemas/subscription_handler_v2_d7369_c10.py
+++ src/schemas/subscription_handler_v2_d7369_c10.py
@@ -1,10 +1,10 @@
-"""SubscriptionHandler schemas v2d7369y2017."""
+"""SubscriptionHandler schemas v2d7369y2018."""
 from dataclasses import dataclass
 from typing import Any, Dict, Optional
 
 
 @dataclass
-class SubscriptionHandlerConfig_v2d7369y2017:
+class SubscriptionHandlerConfig_v2d7369y2018:
     enabled: bool = True
     batch_size: int = 64
     hidden_dim: int = 128
@@ -14,7 +14,7 @@ class SubscriptionHandlerConfig_v2d7369y2017:
     max_epochs: int = 20
 
     @classmethod
-    def from_dict(cls, d: Dict[str, Any]) -> "SubscriptionHandlerConfig_v2d7369y2017":
+    def from_dict(cls, d: Dict[str, Any]) -> "SubscriptionHandlerConfig_v2d7369y2018":
         return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
 
     def validate(self) -> bool:
