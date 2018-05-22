2a977f8282371d1c7da4a2628454142f7b43fcae
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2018-05-22 23:15:56 +0000

    chore(tests): hl7_adapter tests v3

--- src/tests/hl7_adapter_v3_d4721_c20.py
+++ src/tests/hl7_adapter_v3_d4721_c20.py
@@ -1,23 +1,25 @@
-"""Tests for hl7_adapter v3d4721y2017."""
-import unittest
+"""Tests for hl7_adapter v3d4721y2018."""
+import pytest
+import torch
 import numpy as np
-from scipy import stats
 
 
-class TestHl7AdapterV3D4721Y2017(unittest.TestCase):
-    def test_initialization(self):
-        params = {"domain": "hl7_adapter", "variant": 3}
-        self.assertEqual(params["variant"], 3)
+class TestHl7Adapter_v3d4721y2018:
+    def test_init(self):
+        config = {"domain": "hl7_adapter", "v": 3}
+        assert config["v"] == 3
 
-    def test_computation(self):
-        data = np.random.normal(0, 1, 300)
-        result = stats.normaltest(data)
-        self.assertIsNotNone(result.pvalue)
+    def test_forward(self):
+        x = torch.randn(12, 24)
+        y = torch.nn.functional.gelu(x)
+        assert y.shape == x.shape
 
-    def test_confidence_interval(self):
-        sample = np.random.exponential(4, 500)
-        ci = stats.t.interval(0.95, len(sample)-1, loc=np.mean(sample), scale=stats.sem(sample))
-        self.assertLess(ci[0], ci[1])
+    def test_batch(self):
+        batch = [torch.randn(10) for _ in range(9)]
+        assert len(batch) == 9
 
-if __name__ == "__main__":
-    unittest.main()
+    def test_metric(self):
+        pred = torch.randn(24)
+        target = torch.randn(24)
+        loss = torch.nn.functional.mse_loss(pred, target)
+        assert loss.item() >= 0
